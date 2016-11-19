1ecad8a70100a4c5048ca57f88fd973e5bf26adf
ryuryuy <jankdehot@hotmail.com>
2016-11-19 09:04:41 +0000

    added pic and titile

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -18,7 +18,6 @@ class OwnershipsController < ApplicationController
       @item.small_image     = item['smallImageUrls'].first['imageUrl']
       @item.medium_image    = item['mediumImageUrls'].first['imageUrl']
       @item.large_image     = item['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
-      @item.detail_page_url = item['itemUrl']
       @item.save!
     end
 
